19e6b13cb12aca2483f04e6a0294ff44d0df1dca
Krzysztof Sobieraj <Krzysztof.Sobieraj@StepStone.com>
2022-10-10 22:21:54 +0200

    feat(#3): add integration tests for MysqlContext

--- src/RecipesDataProvider.Infrastructure/Database/MysqlContext.cs
+++ src/RecipesDataProvider.Infrastructure/Database/MysqlContext.cs
@@ -13,22 +13,11 @@ public class MysqlContext : IDbContext
 
     public MysqlContext(IConfiguration configuration)
     {
-        // TODO: throw an error is _connectionString is null, empty or is incorrect
-        // server=localhost;port=8083;userid=root;password=root;database=culinary_blog
-        // _connectionString = configuration.GetConnectionString("local");
-        
-        try
-        {
-            _connectionString = configuration.GetConnectionString("local");
+        _connectionString = configuration.GetConnectionString("local");
             
-            if (!ConnectionStringValidator.Validate(_connectionString))
-            {
-                throw new BadConnectionStringException();
-            }
-        }
-        catch (Exception e)
+        if (!ConnectionStringValidator.Validate(_connectionString))
         {
-            throw new Exception(e.Message);
+            throw new BadConnectionStringException();
         }
     }
 
